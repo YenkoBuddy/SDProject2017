06f08cdaa66e94cd3e7398e3e0e9b33b346c4a0d
Kwame10 <frankboateng1992@yahoo.com>
2017-05-27 16:14:26 +0200

    Add files via upload

---
+++ TaxiDriverReg/driverRegistration.php
@@ -0,0 +1,149 @@
+<?php
+	error_reporting(~E_NOTICE);
+    require_once 'include/config.php';
+    require_once 'include/connection.php';
+
+    session_start();
+ 
+    $regNumber = $_POST['RegNo'];
+    $type = $_POST['Type'];
+
+
+     if(isset($_POST['btnFinish']))
+    {
+        $firstname = $_POST['firstname'];
+        $lastName = $_POST['lastName'];
+        $idNo = $_POST['idNo'];
+        $contactNumber = $_POST['contactNumber'];
+        $email = $_POST['email'];
+        $address1 = $_POST['address1'];
+        $suburb = $_POST['suburb'];
+        $res = $_POST['res'];
+        $licence = $_POST['licence'];
+        $prdp = $_POST['prdp'];
+        $headshot = $_POST['headshot'];
+
+        $sql = "INSERT INTO application(FirstName, lastName, IDNo, ContactNumber, EmailAddress, Address1, SuburbID, ProofOfResidence, DriversLicence,PrDP, Photo) 
+            VALUES(:firstname, :lastName, :idNo, :contactNumber, :email, :address1, :suburb, :res, :licence, :prdp, :headshot)";
+
+        $stmt = $conn->prepare($sql);
+
+            $stmt->bindParam(':firstname',$firstname);
+            $stmt->bindParam(':lastName',$lastName);
+            $stmt->bindParam(':idNo',$idNo);
+            $stmt->bindParam(':contactNumber',$contactNumber);
+            $stmt->bindParam(':email',$email);
+            $stmt->bindParam(':address1',$address1);
+            $stmt->bindParam(':suburb',$suburb);
+            $stmt->bindParam(':res',$res);
+            $stmt->bindParam(':licence',$licence);
+            $stmt->bindParam(':prdp',$prdp);
+            $stmt->bindParam(':headshot',$headshot);
+
+             if($stmt->execute())
+            {
+                $successMSG = "new record succesfully inserted ...";
+                header("refresh:5;"); // redirects image view page after 5 seconds.
+            }
+            else
+            {
+                $errMSG = "error while inserting....";
+            }
+    }
+
+   
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Register Taxi</title>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+    <div id="header"><?php include('header.php');?></div>
+<h1><b>Driver Details</b></h1>
+<label>The taxi registered for this driver is: <?php echo $_SESSION['RegNo']; ?> <?php echo $_SESSION['Type']; ?>     
+                </label>
+
+<form method="post">
+    <table>
+        <tr>
+            <td>
+                <input type="text" name="firstname" placeholder="First Name" required="Yes" style="text-align: center"> 
+            </td>
+        <!--&nbsp; &nbsp;--> 
+            <td> 
+                <input type="text" name="lastName" placeholder="last Name" required="Yes" style="text-align: center"> * 
+            </td><td></td>
+        </tr>
+        <tr>
+            <td colspan="3"> 
+                <input type="text" size="47" name="idNo" placeholder="ID Number" required="Yes"
+        style="text-align: center"> *
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3"> 
+                <input type="text" size="47" name="contact" placeholder="Contact Number" required="Yes"
+        style="text-align: center"> *
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3"> <input type="text" size="47" name="email" placeholder="Email Address" required="Yes"
+        style="text-align: center"> *</td>
+    </tr>
+    
+    <tr>
+        <td> 
+            <select name="province" style="WIDTH: 150px" onfocus="this.style.width='100px'" onmouseout="this.style.width='150px'">
+                <option>--Select Province--</option> 
+            </select>
+        </td> 
+        <td> 
+            <select name="city" style="WIDTH: 150px" onfocus="this.style.width='100px'" onmouseout="this.style.width='150px'">
+                <option>--Select City--</option>
+            </select>
+        </td>       
+        <td> 
+            <select name="suburb" style="WIDTH: 150px" onfocus="this.style.width='100px'" onmouseout="this.style.width='150px'">
+                <option>--Select Suburb--</option>
+            </select>* 
+        </td>       
+    
+    </tr>
+    <tr>
+        <td colspan="3"> <input type="text" size="47" name="address" placeholder="Email Address" required="Yes"
+        style="text-align: center"> *</td>
+    </tr>
+    <tr>
+        <td>Proof Of Residence: </td>
+        <td><input type="file" name="roofOfResidence" value="Choose File"> * Type:PDF</td>
+    </tr>
+    <tr>
+        <td>Drivers Licence: </td>
+        <td><input type="file" name="licence" value="Choose File"> * Type:PDF</td>
+    </tr>
+    <tr>
+        <td>PrDP: </td>
+        <td><input type="file" name="prdp" value="Choose File" style="text-align: right;"> * Type:PDF</td>
+    </tr>
+    <tr>
+        <td>Headshot: </td>
+        <td><input type="file" name="headshot" value="Choose File" style="text-align: right;"> *Type:JPG, PNG, JPEG, IMG </td>
+    </tr>
+    <tr>
+        <td><input type="submit" name="btnFinish" value="Finish"></td>
+    </tr>
+</table>
+
+</form>
+<?php if(isset($successMSG)){ ?>
+            <span><?php echo $successMSG; ?></span>
+<?php } ?>
+<?php if(isset($errMSG)){ ?>
+            <span><?php echo $errMSG; ?></span>
+<?php } ?>
+<div id="footer"><?php include_once('footer.php');?></div>
+</body>
+</html>
---
+++ TaxiDriverReg/taxiRegistration.php
@@ -0,0 +1,115 @@
+<?php
+	error_reporting(~E_NOTICE);
+    require_once 'include/config.php';
+    require_once 'include/connection.php';
+
+    session_start();
+    
+    session_destroy();
+    
+    if(isset($_POST['btnNext']))
+    {
+        $regNumber = $_POST['regNumber'];
+        $type = $_POST['type'];
+        $year = $_POST['year'];
+        $model = $_POST['model'];
+        $description = $_POST['description'];
+        $ownerID = "9";
+        $route="1";
+        $licenceDisc = $_POST['licenceDisc'];
+        $inspectionReport = $_POST['inspectionReport'];
+
+        $sql = "INSERT INTO application(RegNo, Type, Year, Model, Description, OwnerID, RouteID) VALUES(:regNumber, :type, :year, :model, :description, :ownerID, :route)";
+        
+        $stmt = $conn->prepare($sql);
+          
+            $stmt->bindParam(':regNumber',$regNumber);
+            $stmt->bindParam(':type',$type);
+            $stmt->bindParam(':year',$year);
+            $stmt->bindParam(':model',$model);
+            $stmt->bindParam(':description',$description);
+            $stmt->bindParam(':ownerID',$ownerID);
+            $stmt->bindParam(':route', $route);
+/*$stmt->bindParam(':licenceDisc',$licenceDisc);
+            $stmt->bindParam(':inspectionReport',$inspectionReport);
+*/
+            $stmt->execute();
+            if($stmt)
+            {
+                $successMSG = "new record succesfully inserted ...";
+                session_start();
+                $_SESSION['RegNo'] = $regNumber;
+                $_SESSION['Type'] = $type;
+                header("Location:driverRegistration.php"); // redirects image view page after 5 seconds.
+            }
+            else
+            {
+                $errMSG = "error while inserting....";
+            }     
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Register Taxi</title>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+    <div id="header"><?php include('header.php');?></div>
+<h1><b>Taxi Details</b></h1>
+<form method="post">
+    <table>
+    <tr>
+        <td> <input type="text" name="regNumber" placeholder="Registration Number" required="Yes" style="text-align: center"> </td>
+        <!--&nbsp; &nbsp;--> 
+        <td> <input type="text" name="type" placeholder="Type" required="Yes" style="text-align: center"> * </td>       
+    </tr>
+    <tr>
+        <td> 
+            <select name="year" style="WIDTH: 170px" onfocus="this.style.width='100px'" onmouseout="this.style.width='170px'"><option>Select Year<?php
+                    $aa = date('Y');
+                    foreach (range(2000, $aa) as $key => $value) 
+                    {
+                        echo "<option>".$value."</option>";
+                        
+                    }
+
+                ?></option>
+                
+            </select>
+        </td>
+        <td> <input type="text" name="model" placeholder="Model" required="Yes"
+        style="text-align: center"> *</td>
+    </tr>
+    
+    <tr>
+        <td colspan="2"><input type="text" size="46" name="description" placeholder="Description" required="Yes" style="text-align: center"> * 
+        </td>
+    </tr>
+    <tr>
+        <td> <select name="startPoint" style="WIDTH: 170px" onfocus="this.style.width='100px'" onmouseout="this.style.width='170px'"><option>Starting Point</option> </select></td> 
+        <td> <select name="endPoint" style="WIDTH: 170px" onfocus="this.style.width='100px'" onmouseout="this.style.width='170px'"><option>Ending Point</option></select>* </td>       
+    </tr>
+    <tr>
+        <td>Motor Vehicle Licence Disc: </td>
+        <td><input type="file" name="licenceDisc" value="Choose File"> *</td>
+    </tr>
+    <tr>
+        <td>Vehicle Inspection Report: </td>
+        <td><input type="file" name="inspectionReport" value="Choose File" style="text-align: right;"> * </td>
+    </tr>
+    <tr>
+        <td><input type="submit" name="btnNext" value="Next"></td>
+    </tr>
+</table>
+
+</form>
+<?php if(isset($successMSG)){ ?>
+            <span><?php echo $successMSG; ?></span>
+<?php } ?>
+<?php if(isset($errMSG)){ ?>
+            <span><?php echo $errMSG; ?></span>
+<?php } ?>
+<div id="footer"><?php include_once('footer.php');?></div>
+</body>
+</html>
